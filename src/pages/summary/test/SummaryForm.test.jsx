37609676ae47459c321c5d215395e915ffb2401e
Morten <mtnielsen89@gmail.com>
2021-02-07 13:10:59 +0100

    Added the summary form and functional tests + clean up

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,9 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import SummaryForm from "../SummaryForm";
 
@@ -26,10 +31,32 @@ test("Check that checking the checkbox enables the button and unchecked disables
   });
 
   // Checking the checkbox and see if the button is enabled
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(confirmOrderButton).toBeEnabled();
 
   // Un-checking the checkbox and see if the button is disabled
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(confirmOrderButton).toBeDisabled();
+});
+
+test("popover response to hover", async () => {
+  render(<SummaryForm />);
+
+  // popover starts out hidden
+  let popover = screen.queryByText(/no ice cream will actually be delivered/i);
+  expect(popover).not.toBeInTheDocument();
+
+  // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+
+  popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions);
+
+  popover = await waitForElementToBeRemoved(() =>
+    screen.getByText(/no ice cream will actually be delivered/i)
+  );
 });
